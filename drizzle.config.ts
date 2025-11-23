188008ea0d3ccfd9e7f9948f11e799863257a056
vodkashot28 <jnalair28@gmail.com>
2025-11-23 09:29:55 +0800

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config(); // Load .env variables
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
